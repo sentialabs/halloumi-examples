2b1f274a2d374796bd31f71bfd7050cd5b3e61f4
Thomas A. de Ruiter <thomas@de-ruiter.cx>
2018-04-13 23:20:51 +0200

    Update 2.1. Resource Anatomy: Resources.rb

--- 2.1. Resource Anatomy: Resources.rb
+++ 2.1. Resource Anatomy: Resources.rb
@@ -20,7 +20,7 @@ A resource can have the following keyword arguments:
 
 Resource types:
   * As a rule of thumb, any CloudFormation resource name prepended by `Halloumi::` should exists.
-  * The CloudFormation resource classes are defined in the halloumi-resources library, see: https://github.com/sentiampc/halloumi-resources/blob/develop/lib/halloumi/resources/aws/s3/bucket.rb
+  * The CloudFormation resource classes are defined in the halloumi-resources library, see for example the [S3 Bucket definition](https://github.com/sentiampc/halloumi-resources/blob/develop/lib/halloumi/resources/aws/s3/bucket.rb).
   * You are free to define your own resources!
 
 The optional code block can be used for configuering the resource, the code block gets called with as aguments the resource object and the index. The resource object can be used to set property and resource values on the resource. The index tells us which resource we are configuering (useful in case we define multiple instances of the same resource).
